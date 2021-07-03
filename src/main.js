12996bfbc451c74e71a8dca79473797ada8e73d2
blithe <1528663970@qq.com>
2021-07-03 19:40:32 +0800

    完成用户列表功能的开发

--- src/main.js
+++ src/main.js
@@ -2,12 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import './assets/font/iconfont.css'
 // 导入全局样式表
 import './assets/css/global.css'
 
 import axios from 'axios'
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  // console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 最后必须返回config
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
